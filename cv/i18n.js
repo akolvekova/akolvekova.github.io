80c26b302279f83e0dbc805472b450be972ec1db
kolvekova ada <kolvekova@novitechgroup.sk>
2021-01-26 07:41:43 +0100

    cv - jazykove verzie, tlac

---
+++ cv/i18n.js
@@ -0,0 +1,384 @@
+ var cvJson = {
+    "cv": {
+        "header": {
+            "title": {
+                "en": "R e s u m e",
+    			"sk": "Ž i v o t o p i s"
+		    },
+		    "name": {
+			    "en": "ANDREA KOLVEKOVA",
+			    "sk": "ANDREA KOĽVEKOVÁ"
+		    },
+		    "job": {
+			    "en": "Programmer",
+			    "sk": "Programátor"
+            }
+        },
+		"about-me": {
+			"title": {
+				"en": " ABOUT ME",
+				"sk": " O MNE"
+			},
+			"text-1": {
+				"en": "I am a programmer and I love my job. Really.",
+				"sk": "Som programátor a milujem svoju prácu. Naozaj."
+			},
+			"text-2": {
+				"en": "Responsibility, reliability, positive approach to problem solving, accuracy, communication, teamwork and the ability to acquire new skills are my strengths.",
+				"sk": "Zodpovednosť, spoľahlivosť, pozitívny prístup k riešeniu problémov, presnosť, komunikácia, tímová práca a schopnosť získavať nové zručnosti sú moje silné stránky."
+			}
+        },
+        "education": {
+            "title": {
+                "en": " EDUCATION",
+    			"sk": " VZDELANIE"
+		    },
+		    "text-1": {
+			    "en": "Pavol Jozef Šafárik University, Kosice - Faculty of Science",
+			    "sk": "Univerzita Pavla Jozefa Šafárika, Košice, Prírodovedecká fakulta"
+		    },
+		    "text-1a": {
+			    "en": "Informatics",
+			    "sk": "Informatika"
+            },
+            "text-2": {
+			    "en": "Pavol Jozef Šafárik University, Kosice - Faculty of Science",
+			    "sk": "Univerzita Pavla Jozefa Šafárika, Košice, Prírodovedecká fakulta"
+		    },
+		    "text-2a": {
+			    "en": "Mathematics - Chemistry",
+			    "sk": "Matematika - Chémia"
+            },
+            "text-3": {
+			    "en": "Martin Kukučín Gymnázium, Revuca",
+			    "sk": "Gymnázium Martina Kukučína, Revúca"
+		    }
+        },
+        "skills": {
+            "title": {
+                "en": " SKILLS",
+    			"sk": " ZRUČNOSTI"
+		    },
+		    "en": {
+			    "en": "English",
+			    "sk": "Angličtina"
+		    },
+		    "ru": {
+			    "en": "Russian",
+			    "sk": "Ruština"
+            },
+            "de": {
+			    "en": "German",
+			    "sk": "Nemčina"
+		    }
+        },
+        "experience": {
+            "title": {
+                "en": " WORK EXPERIENCE",
+    			"sk": " PRACOVNÉ SKÚSENOSTI"
+		    },
+		    "text": {
+			    "en": "Novitech Tax, s.r.o., Kosice",
+			    "sk": "Novitech Tax, s.r.o., Košice"
+		    },
+		    "text-a": {
+			    "en": "Web applications development in Java, PHP, Angular",
+			    "sk": "Vývoj webových aplikácií v jazykoch Java, PHP, Angular"
+		    },
+		    "text-b": {
+			    "en": "4GL procedures development",
+			    "sk": "Vývoj 4GL procedúr"
+            },
+            "cards": {
+                "vref": {
+                    "subscription": {
+                        "en": "EU VAT refund support system",
+			            "sk": "Systém podpory vrátenia DPH v EÚ"
+                    },
+                    "customer": {
+                        "en": "FR SR",
+			            "sk": "FR SR"
+                    },
+                    "text-1": {
+                        "en": "Web application development and maintenance (JSF, Java 8, Oracle)",
+			            "sk": "Vývoj a údržba webovej aplikácie (JSF, Java 8, Oracle)"
+                    }
+                },
+                "satesop": {
+                    "subscription": {
+                        "en": "The Warehouses Module",
+			            "sk": "Modul Sklady"
+                    },
+                    "customer": {
+                        "en": "SKODA, CR",
+			            "sk": "ŠKODA, ČR"
+                    },
+                    "text-1": {
+                        "en": "Project frontend implementation (Angular 5, Bootstrap 4)",
+			            "sk": "Implementácia frontendu projektu (Angular 5, Bootstrap 4)"
+                    }
+                },
+                "cbc": {
+                    "subscription": {
+                        "en": "Cross-border HU-SK virtual tours",
+			            "sk": "Cezhraničné virtuálne prehliadky HU-SK"
+                    },
+                    "customer": {
+                        "en": "Novitech, a.s., Kosice",
+			            "sk": "Novitech, a.s., Košice"
+                    },
+                    "text-1": {
+                        "en": "Project frontend implementation (Bootstrap 4, jQuery 3)",
+			            "sk": "Implementácia frontendu projektu (Bootstrap 4, jQuery 3)"
+                    }
+                },
+                "aisse": {
+                    "subscription": {
+                        "en": "IS of personal documents in the Czech Republic - module Civic Cards",
+			            "sk": "IS osobných dokladov v Českej republike - modul Občianske preukazy"
+                    },
+                    "customer": {
+                        "en": "KOMIX, CR",
+			            "sk": "KOMIX, ČR"
+                    },
+                    "text-1": {
+                        "en": "Project backend implementation (Java 8)",
+			            "sk": "Implementácia backendu projektu (Java 8)"
+                    }
+                },
+                "i3d": {
+                    "subscription": {
+                        "en": "Application of virtual and augmented reality in industrial smart workplaces",
+			            "sk": "Aplikácia virtuálnej a rozšírenej reality na inteligentných priemyselných pracoviskách"
+                    },
+                    "customer": {
+                        "en": "Novitech, a.s., Kosice",
+			            "sk": "Novitech, a.s., Košice"
+                    },
+                    "text-1": {
+                        "en": "Project frontend implementation (Angular 5, Bootstrap 3)",
+			            "sk": "Implementácia frontendu projektu (Angular 5, Bootstrap 3)"
+                    },
+                    "text-2": {
+                        "en": "Project backend implementation (PHP)",
+			            "sk": "Implementácia backendu projektu (PHP)"
+                    }
+                },
+                "usde": {
+                    "subscription": {
+                        "en": "The management of the electricity demand and distribution system",
+			            "sk": "Aplikácia na riadenie dopytu po elektrine a distribučnej sústavy"
+                    },
+                    "customer": {
+                        "en": "Východoslovenská distribučná, a.s., Kosice",
+			            "sk": "Východoslovenská distribučná, a.s., Košice"
+                    },
+                    "text-1": {
+                        "en": "Project frontend implementacion (Angular 5, Bulma)",
+			            "sk": "Implementácia frontendu projektu (Angular 5, Bulma)"
+                    },
+                    "text-2": {
+                        "en": "Project backend implementation (Firebase Realtime Database)",
+			            "sk": "Implementácia backendu projektu (Firebase Realtime Database)"
+                    },
+                    "text-3": {
+                        "en": "Implementation of the main website",
+			            "sk": "Implementácia hlavnej webovej stránky"
+                    }
+                },
+                "nuvia": {
+                    "subscription": {
+                        "en": "Virtual tours web application",
+			            "sk": "Webová aplikácia pre virtuálne prehliadky"
+                    },
+                    "customer": {
+                        "en": "Novitech, a.s., Kosice",
+			            "sk": "Novitech, a.s., Košice"
+                    },
+                    "text-1": {
+                        "en": "Project frontend implementation (Bootstrap 3)",
+			            "sk": "Implementácia frontendu projektu (Bootstrap 3)"
+                    },
+                    "text-2": {
+                        "en": "Project backend implementation (PHP)",
+			            "sk": "Implementácia backendu projektu (PHP)"
+                    }
+                },
+                "exis": {
+                    "subscription": {
+                        "en": "Information system for distrainers",
+			            "sk": "Informačný systém pre exekútorov"
+                    },
+                    "customer": {
+                        "en": "Exekútorský úrad Kováč Ladislav, Kosice",
+			            "sk": "Exekútorský úrad Kováč Ladislav, Košice"
+                    },
+                    "text-1": {
+                        "en": "Implementation of project modules (Java)",
+			            "sk": "Implementácia modulov projektu (Java)"
+                    }
+                },
+                "apvdis": {
+                    "subscription": {
+                        "en": "Tax information system of Slovak Republic",
+			            "sk": "Daňový informačný systém Slovenskej republiky"
+                    },
+                    "customer": {
+                        "en": "Tax Directorate of the Slovak Republic",
+			            "sk": "Daňové riaditeľstvo Slovenskej republiky"
+                    },
+                    "text-1": {
+                        "en": "Report programming (4gl)",
+			            "sk": "Programovanie reportov (4gl)"
+                    }
+                }
+            },
+            "table": {
+                "project": {
+                    "en": "Project",
+			        "sk": "Projekt"
+                },
+                "subscription": {
+                    "en": "Subscription",
+                    "sk": "Popis"
+                },
+                "customer": {
+                    "en": "Customer",
+                    "sk": "Zákazník"
+                },
+                "specification": {
+                    "en": "Specification",
+                    "sk": "Špecifikácia"
+                },
+                "years": {
+                    "en": "Years",
+                    "sk": "Roky"
+                }
+            }
+        },
+        "supplement": {
+            "title": {
+                "en": " SUPPLEMENT",
+    			"sk": " PRÍLOHA"
+		    },
+		    "text": {
+			    "en": "My learning project as an example of my work",
+			    "sk": "Môj vzdelávací projekt ako príklad mojej práce"
+		    }
+        },
+        "contacts": {
+            "title": {
+                "en": " CONTACTS",
+    			"sk": " KONTAKTY"
+		    },
+		    "address-1": {
+			    "en": "04018 Kosice",
+			    "sk": "04018 Košice"
+            },
+            "address-2": {
+			    "en": "Slovakia",
+			    "sk": "Slovenská republika"
+		    }
+        }
+	}
+};
+
+(function () {
+	this.I18n = function (defaultLang) {
+		var lang = defaultLang || 'en';
+		this.language = lang;
+
+		(function (i18n) {
+			i18n.contents = cvJson;
+			i18n.contents.prop = function (key) {
+				var result = this;
+				var keyArr = key.split('.');
+				for (var index = 0; index < keyArr.length; index++) {
+					var prop = keyArr[index];
+					result = result[prop];
+				}
+				return result;
+			};
+			i18n.localize();
+		})(this);
+	};
+
+	this.I18n.prototype.hasCachedContents = function () {
+		return this.contents !== undefined;
+	};
+
+	this.I18n.prototype.lang = function (lang) {
+		if (typeof lang === 'string') {
+			this.language = lang;
+		}
+		this.localize();
+		return this.language;
+	};
+
+	this.I18n.prototype.localize = function () {
+		var contents = this.contents;
+		if (!this.hasCachedContents()) {
+			return;
+		}
+		var dfs = function (node, keys, results) {
+			var isLeaf = function (node) {
+				for (var prop in node) {
+					if (node.hasOwnProperty(prop)) {
+						if (typeof node[prop] === 'string') {
+							return true;
+						}
+					}
+				}
+			}
+			for (var prop in node) {
+				if (node.hasOwnProperty(prop) && typeof node[prop] === 'object') {
+					var myKey = keys.slice();
+					myKey.push(prop);
+					if (isLeaf(node[prop])) {
+						results.push(myKey.reduce( function (previousValue, currentValue, currentIndex, array) {
+							return previousValue + '.' + currentValue;
+						}));
+					} else {
+						dfs(node[prop], myKey, results);
+					}
+				}
+			}
+			return results;
+		};
+		var keys = dfs(contents, [], []);
+		for (var index = 0; index < keys.length; index++) {
+			var key = keys[index];
+			if (contents.prop(key).hasOwnProperty(this.language)) {
+				$('[data-i18n="'+key+'"]').text(contents.prop(key)[this.language]);
+				$('[data-i18n-placeholder="'+key+'"]').attr('placeholder', contents.prop(key)[this.language]);
+				$('[data-i18n-value="'+key+'"]').attr('value', contents.prop(key)[this.language]);
+			} else {
+				$('[data-i18n="'+key+'"]').text(contents.prop(key)['en']);
+				$('[data-i18n-placeholder="'+key+'"]').attr('placeholder', contents.prop(key)['en']);
+				$('[data-i18n-value="'+key+'"]').attr('value', contents.prop(key)['en']);
+			}
+		}
+	};
+
+}).apply(window);
+
+$( document ).ready( function () {
+
+	var i18n = new I18n();
+	i18n.localize();
+	$('.lang-picker #en').addClass('selected');
+	
+	$('.lang-picker #en').on('click', function () {
+		i18n.lang('en');
+		selectLang($(this));
+	})
+	$('.lang-picker #sk').on('click', function () {
+		i18n.lang('sk');
+		selectLang($(this));
+	})
+
+	function selectLang (picker) {
+		$('.lang-picker li').removeClass('selected');
+		picker.addClass('selected');
+	}
+});
